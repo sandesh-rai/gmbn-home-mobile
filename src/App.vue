8b386ca8ba688167172002003730646cc0f274f5
S Rai <sandesh.thulung.rai@gmail.com>
2022-10-25 02:36:29 +0100

    Show warning message on desktop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <AppHeader @archiveSelected="(val) => selectedArchive = val"/>
+    <p class="desktop-message">{{'Please view this page on mobile with Chrome DevTools. It has not been optimised for tablet or desktop.'}}</p>
     <HomePage :selectedArchive="selectedArchive"/>
   </div>
 </template>
@@ -27,5 +28,19 @@ export default {
 body {
   margin: 0;
   font-family: proxima-nova, sans-serif;
+}
+</style>
+
+<style scoped>
+p.desktop-message {
+  margin: 1rem;
+  padding: 1rem;
+  border: 4px solid red;
+}
+
+@media only screen and (max-width: 500px) {
+  p.desktop-message {
+    display: none;
+  }
 }
 </style>
